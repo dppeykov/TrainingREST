ce1a98f807939e8158eae3f72afbf6d4aa60a8fc
dpeykov <dpeykov@netsuite.com>
2021-01-17 15:19:28 +0100

    Before using the actual Django REST API framework

--- firstApp/urls.py
+++ firstApp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     # goes to http://localhost:8000/firstApp/test_emp
-    path('test_emp', views.employeeDictView),
+    path('test_emp/', views.employeeDictView, name='emp_dict'),
+    path('emps/', views.employeeDbView, name='emps_db'),
 ]
--- firstApp/views.py
+++ firstApp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .models import Employee
+
+# ------ NOT USING THE DJANGO REST API FRAMEWORK, BUT JUST THE DJANGO WEB FRAMEWORK ---------------
+# ------ Used just as an example how the web framework is handling such requests ------------------
 
 def employeeDictView(request):
     emp = {
@@ -10,3 +14,12 @@ def employeeDictView(request):
 
     # JsonResponse is a Django serializer - it will retun the dictionary emp into JSON
     return JsonResponse(emp)
+
+def employeeDbView(request):
+
+    data = Employee.objects.all()
+    response = {'employees': list(data.values('name', 'sal'))}
+
+    return JsonResponse(response)
+
+# --------------------------------------------------------------------------------------------------
